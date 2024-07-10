1105e4ba77cd23150b9c032674a78ac8a81d9102
goutham120203 <gouthampaladugu12@gmail.com>
2024-07-10 23:28:48 +0530

    hgvgh

---
+++ src/first copy.java
@@ -0,0 +1,14 @@
+public class first {
+    public static void main(String[] args) {
+        int count = 10;
+        System.out.println("count is "+count);
+        count = 20;
+        System.out.println("2nd count is "+count);
+        double interest = 2.3;
+        String name = "goutham";
+        System.out.println("the interest is "+interest);
+        System.out.println("name is "+name);
+
+    }
+    
+}
